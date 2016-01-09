6b9fc051c17487eef9a4637feb600dbfeaa5f33e
Andrei Gliga <otzy_007@yahoo.com>
2016-01-09 15:43:34 +0200

    map for place

--- app/helpers/places_helper.rb
+++ app/helpers/places_helper.rb
@@ -1,3 +1,19 @@
 module PlacesHelper
-
+  def gmap(place)
+    (
+      "<div style='width: 800px;'><div id=\"#{place.id}\" style='width: 800px; height: 400px;'></div></div>" +
+      javascript_tag("#{place.id} = Gmaps.build('Google');
+      #{place.id}.buildMap({ provider: {}, internal: {id: '#{place.id}'}}, function(){
+      markers = #{place.id}.addMarkers([
+        {
+          \"lat\": #{place.lat},
+          \"lng\": #{place.long},
+          \"infowindow\": \"#{place.name}\"
+        }
+      ]);
+      #{place.id}.bounds.extendWith(markers);
+      #{place.id}.fitMapToBounds();
+      });")
+    ).html_safe
+  end
 end
